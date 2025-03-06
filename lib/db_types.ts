97ed2d2be77a1bc6c779a14427919a47e2e3c673
Anirban Majumder <81350357+Anirban-Majumder@users.noreply.github.com>
2025-03-06 12:17:58 +0530

    added syllabus processing

--- lib/db_types.ts
+++ lib/db_types.ts
@@ -3,12 +3,12 @@ export interface Profile {
     created_at: string; // ISO timestamp format
     first_name: string;
     last_name: string;
-    subjects: Record<string, any>; // JSON structure (adjust type accordingly)
-    smt_details: Record<string, any>; // JSON structure
-    grade: Record<string, any>; // JSON structure
-    tech_proficiency: Record<string, any>; // JSON structure
-    learning_style: Record<string, any>; // JSON structure
-    learning_challenges: Record<string, any>; // JSON structure
+    subjects: any; // JSON structure (adjust type accordingly)
+    smt_details: any; // JSON structure
+    grade: string;
+    tech_proficiency: string;
+    learning_style: string;
+    learning_challenges: string[];
     user_id: string; // UUID
 }
   
